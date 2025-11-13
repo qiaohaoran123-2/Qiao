9f7c2227d840a1a766e3ea9365e1e88ba4e4ff21
qiaohaoran123-2 <qiaohaoran123-1@outlook.com>
2025-11-13 20:41:58 +0800

    终于写完了

--- CSP-J 2025/seat/seat.cpp
+++ CSP-J 2025/seat/seat.cpp
@@ -1,8 +1,34 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#define code return
+#define by 0
+#define qiaohaoran ;
 using namespace std;
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
+long long n, m, R, a[100005], t[105], top;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> m;
+	for (int i = 1; i <= n * m; i++){
+		cin >> a[i];
+		t[a[i]]++;
+	}
+	R = a[1];
+	for (int i = 100; i >= 0; i--) {
+		if (t[i]) top++;
+		if (i == R) {
+			R = top;
+			break;
+		}
+	}
+	int c = (R % n == 0 ? n : R % n);
+	int x = 0, y = (R / n) + (R % n != 0);
+	if (y % 2 == 0) {
+		x = n - c + 1;
+	}
+	else {
+		x = c;
+	}
+	cout << y << ' ' << x;
+	code by qiaohaoran
 }
